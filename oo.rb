c73059e214cf148f7e6c25a67ff18d0c575a3f23
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-10 15:46:37 -0400

    first commit

---
+++ oo.rb
@@ -0,0 +1,35 @@
+#Class (umbrella)
+#Instances state
+
+class BankAccount
+
+  def initialize(balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+  def balance()
+    return @balance
+  end
+
+  def interest_rate()
+    return @interest_rate
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def gain_interest()
+    @balance *= (interest_rate + 1)
+  end
+
+
+
+end
+
+person1 = BankAccount.new(200, 1.13)
